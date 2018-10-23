ab41b3183a0ea6b4c9110f5e7676ead1217bdc76
Takahiro Kato <takahiro.kato15@gmail.com>
2018-10-23 23:58:43 +0900

    Initial Commit

---
+++ CodingTestSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodingTestSample
+//
+//  Created by Takahiro Kato on 2018/10/23.
+//  Copyright © 2018 Takahiro Kato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
